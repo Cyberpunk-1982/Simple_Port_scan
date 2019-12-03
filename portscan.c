5803c005fa8689cc31470ec94ee1a719e8d1ceb7
Eliseu <eliseutorres.gx@gmail.com>
2019-12-03 10:39:39 -0300

    Create portscan.c

---
+++ portscan.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <netdb.h>
+
+#define MAX_NUM_PORT 65535
+
+int
+main(int argc, char **argv)
+{
+	struct sockaddr_in addr;
+	int sfd, cfd;
+	int port;
+	char *host;
+	
+	if (argc < 2) {
+		printf("IP address is missing!\n");
+		exit(-1);
+	}
+	
+	host = argv[1];
+	printf("Port	Status\n");
+	for (port = 1; port < MAX_NUM_PORT; port++) {
+		sfd = socket(AF_INET, SOCK_STREAM, 0);
+		if (sfd == -1) {
+			printf("socket error\n");
+			exit(-1);
+		}
+		
+		addr.sin_family = AF_INET;
+		addr.sin_port = htons(port);
+		addr.sin_addr.s_addr = inet_addr(host);
+		
+		cfd = connect(sfd, (struct sockaddr *) &addr, sizeof addr);
+		if (cfd == 0) 
+			printf("%i	Open\n", port);				
+		
+		close(sfd);
+		close(cfd);
+	}
+	
+	exit(EXIT_SUCCESS);
+} 
